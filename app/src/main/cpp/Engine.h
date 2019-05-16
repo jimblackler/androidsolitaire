1228b03833fa478287625603e0513dee7c0dafa6
Jim Blackler <jimblackler@google.com>
2019-05-16 14:44:39 +0100

    Bug fix.

--- app/src/main/cpp/Engine.h
+++ app/src/main/cpp/Engine.h
@@ -3,6 +3,7 @@
 
 class Engine {
 public:
+  virtual ~Engine() = default;
   virtual int32_t handleInput(AInputEvent *pEvent) = 0;
   virtual void handleCmd(int32_t cmd) = 0;
   virtual void mainLoop() = 0;
