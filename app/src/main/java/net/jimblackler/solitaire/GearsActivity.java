95e6461250fa2f245834548463d49d4600fb80e4
Jim Blackler <jimblackler@google.com>
2019-05-23 10:54:41 +0100

    Menu changes.

--- app/src/main/java/net/jimblackler/solitaire/GearsActivity.java
+++ app/src/main/java/net/jimblackler/solitaire/GearsActivity.java
@@ -3,22 +3,19 @@ package net.jimblackler.solitaire;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
 public class GearsActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.activity_gears);
     findViewById(R.id.new_game).setOnClickListener(v -> {
       setResult(RESULT_OK, new Intent().putExtra("action", "newGame"));
       finish();
     });
-    findViewById(R.id.quit).setOnClickListener(v -> {
-      setResult(RESULT_OK, new Intent().putExtra("action", "quit"));
-      finish();
-    });
-
     Common.setDecorView(getWindow().getDecorView());
   }
 }
